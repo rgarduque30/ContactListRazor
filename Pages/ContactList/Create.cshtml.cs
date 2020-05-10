f1573c257ee90dc1004fa83a752a348e85cba882
Rodolfo Garduque <rgarduque30@live.com>
2020-05-10 16:53:50 +0800

    Added Create and Update operations

---
+++ Pages/ContactList/Create.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactListRazor.Pages.ContactList
+{
+    public class CreateModel : PageModel
+    {
+        private ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Contact Contact { get; set; }
+        public async Task OnGet(int id)
+        {
+            Contact = await _db.Contact.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Contact.AddRange(Contact);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
---
+++ Pages/ContactList/Edit.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactListRazor.Pages.ContactList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Contact Contact { get; set; }
+        public async Task OnGet(int id)
+        {
+            Contact = await _db.Contact.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var contact = await _db.Contact.FindAsync(id);
+                contact.ID = Contact.ID;
+                contact.FirstName = Contact.FirstName;
+                contact.MiddleName = Contact.MiddleName;
+                contact.LastName = Contact.LastName;
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else 
+            {
+                return Page();
+            }
+        }
+    }
+}
